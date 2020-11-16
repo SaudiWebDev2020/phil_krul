5fc1f8389e0dd632b975b4524f5ce168f74ec94a
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-11-15 23:57:34 -0600

    adding w6d2 challenges

--- algorithms/weekSix/dayTwo.py
+++ algorithms/weekSix/dayTwo.py
@@ -1,13 +1,13 @@
-Recursive Fibonacci
-------------------------------------------------------------------------------------------------------------------------
-Write rFib(num). Recursively compute and return the numth Fibonacci value. As earlier, treat the first two (num = 0, num = 1) Fibonacci values as 0 and 1. Thus:
-rFib(2) = 1 (0+1)
-rFib(3) = 2 (1+1)
-rFib(4) = 3 (1+2)
-rFib(5) = 5 (2+3)
-rFib(3.65) = rFib(3) = 2
-rFib(-2) = rFib(0) = 0.
+# Recursive Fibonacci
+# ------------------------------------------------------------------------------------------------------------------------
+# Write rFib(num). Recursively compute and return the numth Fibonacci value. As earlier, treat the first two (num = 0, num = 1) Fibonacci values as 0 and 1. Thus:
+# rFib(2) = 1 (0+1)
+# rFib(3) = 2 (1+1)
+# rFib(4) = 3 (1+2)
+# rFib(5) = 5 (2+3)
+# rFib(3.65) = rFib(3) = 2
+# rFib(-2) = rFib(0) = 0.
 
-rListLength
-------------------------------------------------------------------------------------------------------------------------
-Given the first node of a singly linked list, create a recursive function that returns the number of nodes in that list. You can assume the list contains no loops, and that it is short enough that you will not ‘blow your stack’.
+# rListLength
+# ------------------------------------------------------------------------------------------------------------------------
+# Given the first node of a singly linked list, create a recursive function that returns the number of nodes in that list. You can assume the list contains no loops, and that it is short enough that you will not ‘blow your stack’.
