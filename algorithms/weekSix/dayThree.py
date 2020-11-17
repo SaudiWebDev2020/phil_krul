c884c1113a8aa89ea2a1b21d73595a21edddd9a6
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-11-17 00:02:48 -0600

    adding w6d3 challenges

---
+++ algorithms/weekSix/dayThree.py
@@ -0,0 +1,4 @@
+# Binary String Expansion
+# ---------------------------------------------------------------------------------
+# You will be given a string containing characters ‘0’, ‘1’, and ‘?’. For every ‘?’, either ‘0’ or ‘1’ characters can be substituted. Write a recursive function that returns an array of all valid strings that have ‘?’ characters expanded into ‘0’ or ‘1’.
+# Ex.: binStrExpand("1?0?") should return ["1000","1001","1100","1101"]. For this challenge, you can use string functions such as slice(), etc., but be frugal with their use, as they are expensive.
