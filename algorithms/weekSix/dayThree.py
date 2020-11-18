b5ec29beecb57ff9552f243a225de527d6a0de7d
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-11-17 23:43:15 -0600

    adding w6d4 algo challenge

--- algorithms/weekSix/dayThree.py
+++ algorithms/weekSix/dayThree.py
@@ -2,3 +2,16 @@
 # ---------------------------------------------------------------------------------
 # You will be given a string containing characters ‘0’, ‘1’, and ‘?’. For every ‘?’, either ‘0’ or ‘1’ characters can be substituted. Write a recursive function that returns an array of all valid strings that have ‘?’ characters expanded into ‘0’ or ‘1’.
 # Ex.: binStrExpand("1?0?") should return ["1000","1001","1100","1101"]. For this challenge, you can use string functions such as slice(), etc., but be frugal with their use, as they are expensive.
+my_str = '1?0'
+r_list = 
+
+my_str = '?101010'
+r_list = ['0101010','1101010']
+
+my_str_2 = '??'
+
+
+def binStrExpand(input_str, temp_str = '', index = 0, return_list = []):
+    pass
+
+binStrExpand(my_str)
